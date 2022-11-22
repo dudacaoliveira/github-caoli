7ddcd43ca3c086b36a9d59acf258b2e7f2589479
Carlos Eduardo <dudacaoli@gmail.com>
2022-11-22 14:01:33 -0300

    Commit

---
+++ discomed_v1/lib/Cores.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color corFonte = Colors.white;
+Color corFontAppBar = Colors.white;
+Color corIcone = Colors.white;
+Color corIconeMenu = Colors.black;
+Color corBckgScaffold = Colors.grey[200];
+
+
+
+
